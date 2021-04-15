9028a22db8650c7be0c1ae732fb73476311a9956
tumb1er <zimbler@gmail.com>
2021-04-15 13:51:51 +0300

    Add a test for read-only create fields

--- testproject/testapp/models.py
+++ testproject/testapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -8,3 +9,4 @@ class Project(models.Model):
 class Task(models.Model):
     project = models.ForeignKey(Project, models.CASCADE)
     name = models.CharField(max_length=32)
+    author = models.ForeignKey(User, models.CASCADE)
