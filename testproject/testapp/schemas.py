475787fb647a030e98bf462e61afb21562a56347
tumb1er <zimbler@gmail.com>
2021-04-28 11:45:52 +0300

    Use simplified schema in tests

--- testproject/testapp/schemas.py
+++ testproject/testapp/schemas.py
@@ -1,21 +1,18 @@
 PROJECT_TASK_SCHEMA = {
-    "id": {"type": "number"},
-    "name": {"type": ["string", "null"]},
-    "author": {"type": ["number"]},
+    "id": int,
+    "name": [None, str],
+    "author": int,
 }
 
 PROJECT_SCHEMA = {
-    "id": {"type": ["number"]},
-    "name": {"type": ["string"]},
-    "task_set": {
-        "type": "array",
-        "items": PROJECT_TASK_SCHEMA,
-    }
+    "id": int,
+    "name": str,
+    "task_set": [PROJECT_TASK_SCHEMA]
 }
 
 TASK_SCHEMA = {
-    "id": {"type": "number"},
-    "project": {"type": "number"},
-    "name": {"type": ["string", "null"]},
-    "author": {"type": ["number"]},
+    "id": int,
+    "project": int,
+    "name": [None, str],
+    "author": int,
 }
