e0e9f5bc8821eb0cacbcd3d9b84f494ed1c478ed
tumb1er <zimbler@gmail.com>
2021-04-28 11:31:40 +0300

    Add typing_extensions for python3.6 in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import re
 import subprocess
+import sys
 from pathlib import Path
 
 from setuptools import setup  # type: ignore
@@ -58,6 +59,14 @@ def get_version():
     return version
 
 
+install_requires = [
+    'Django',
+    'djangorestframework',
+    'jsonschema',
+]
+if sys.version_info < (3, 7):
+    install_requires.append("typing_extensions")
+
 setup(
     name=package_name,
     version=get_version() or 'dev',
@@ -69,11 +78,7 @@ setup(
     author='Sergey Tikhonov',
     author_email='zimbler@gmail.com',
     description='Smoke tests for API built with Django Rest Framework',
-    install_requires=[
-        'Django',
-        'djangorestframework',
-        'jsonschema',
-    ],
+    install_requires=install_requires,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
