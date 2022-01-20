a26223b74147f19dd138efbde23d86b7f7fd4967
tumb1er <zimbler@gmail.com>
2022-01-20 19:14:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
